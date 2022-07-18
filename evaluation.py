97e2edd0385796eb6c0236de454add78e77cf26b
Lukas -skywall- Sztefek <lukas.sztefek@gmail.com>
2022-07-18 14:27:53 +0200

    feat: Prepare for Experiment #4 (dataset shuffle, simplify model architecture)

---
+++ evaluation.py
@@ -0,0 +1,103 @@
+import csv
+from collections import defaultdict
+
+import numpy as np
+import pandas
+import tensorflow
+from matplotlib import pyplot as plt
+
+from config import BATCH_SIZE
+from dataset import XRayDatasetGenerator
+from train import create_model
+
+
+def generate_submission():
+    dataset_eval = XRayDatasetGenerator(
+        "dataset_generated/test", "dataset_original/sample_submission.csv", training=False
+    ).get_dataset().batch(BATCH_SIZE)
+
+    model = create_model()
+
+    latest = tensorflow.train.latest_checkpoint("checkpoints/")
+    model.load_weights(latest)
+
+    uids = []
+    targets = []
+
+    for (x, labels) in dataset_eval:
+        preds = model(x)
+        preds = np.rint(preds)
+
+        for label, pred in zip(labels, preds):
+            pred_label = pred.nonzero()[0]
+            pred_label = pred_label + 1  # labels indexing starts from 1
+
+            uids.append(label.numpy().decode())
+            targets.append(" ".join(map(str, pred_label)))
+
+    data = {
+        "SOPInstanceUID": uids,
+        "Target": targets
+    }
+
+    df = pandas.DataFrame(data)
+    df.to_csv("submission.csv", index=False, quoting=csv.QUOTE_NONE)
+
+
+def evaluate_on_training_dataset():
+    dataset_train = XRayDatasetGenerator(
+        "dataset_generated/train", "dataset_original/train.csv", training=True
+    ).get_dataset().batch(BATCH_SIZE)
+
+    model = create_model()
+
+    latest = tensorflow.train.latest_checkpoint("checkpoints/")
+    model.load_weights(latest)
+
+    total = defaultdict(int)
+    correct = defaultdict(int)
+    incorrect = defaultdict(int)
+
+    for (x, y) in dataset_train:
+        preds = model(x)
+        preds = np.rint(preds)
+
+        for y, pred in zip(y, preds):
+            y_label = np.array(y).nonzero()[0]
+            y_label = y_label + 1  # labels indexing starts from 1
+
+            pred_label = pred.nonzero()[0]
+            pred_label = pred_label + 1
+
+            pred_labels = ",".join(map(str, pred_label))
+            truth_labels = ",".join(map(str, y_label))
+
+            if pred_labels == truth_labels:
+                total[pred_labels] = total[pred_labels] + 1
+                correct[pred_labels] = correct[pred_labels] + 1
+            else:
+                total[truth_labels] = total[truth_labels] + 1
+                incorrect[truth_labels + "->" + pred_labels] = incorrect[truth_labels + "->" + pred_labels] + 1
+
+    total = dict(sorted(total.items()))
+    correct = dict(sorted(correct.items()))
+    incorrect = dict(sorted(incorrect.items()))
+
+    _, arr = plt.subplots(3, 1)
+    bars = arr[0].bar(total.keys(), total.values())
+    arr[0].set_title("Total (" + str(sum(total.values())) + ")")
+    arr[0].bar_label(bars)
+    bars = arr[1].bar(correct.keys(), correct.values())
+    arr[1].set_title("Correctly classified (" + str(sum(correct.values())) + ")")
+    arr[1].bar_label(bars)
+    bars = arr[2].bar(incorrect.keys(), incorrect.values())
+    arr[2].set_title("Incorrectly classified [truth->prediction] (" + str(sum(incorrect.values())) + ")")
+    arr[2].bar_label(bars)
+    arr[2].tick_params(labelrotation=45)
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    # generate_submission()
+    evaluate_on_training_dataset()
--- train.py
+++ train.py
@@ -1,12 +1,10 @@
-import csv
 import os
 from datetime import datetime
 
 import keras
 import numpy as np
-import pandas
 import tensorflow
-from keras import layers, optimizers, losses
+from keras import layers, optimizers, losses, callbacks
 from keras.applications.resnet_rs import ResNetRS50
 
 from config import LABEL_COUNT, LEARNING_RATE, EPOCHS, BATCH_SIZE, INPUT_IMAGE_SIZE
@@ -25,7 +23,6 @@ def create_model():
     model = keras.Sequential([
         backbone,
         layers.GlobalAveragePooling2D(),
-        layers.Dense(128, activation="relu"),
         layers.Dense(LABEL_COUNT, activation="sigmoid")
     ])
 
@@ -42,8 +39,8 @@ def train(load_weights=False):
     generator_train = XRayDatasetGenerator("dataset_generated/train", "dataset_original/train.csv")
 
     dataset_train = generator_train.get_dataset()
-    dataset_val = dataset_train.take(150).batch(BATCH_SIZE)
-    dataset_train = dataset_train.skip(150).batch(BATCH_SIZE)
+    dataset_val = dataset_train.take(340).batch(BATCH_SIZE)
+    dataset_train = dataset_train.skip(340).shuffle(BATCH_SIZE * 10, reshuffle_each_iteration=True).batch(BATCH_SIZE)
 
     model = create_model()
 
@@ -51,7 +48,7 @@ def train(load_weights=False):
         latest = tensorflow.train.latest_checkpoint("checkpoints/")
         model.load_weights(latest)
 
-    cp_callback = tensorflow.keras.callbacks.ModelCheckpoint(
+    cp_callback = callbacks.ModelCheckpoint(
         filepath="checkpoints/checkpoint.cp",
         save_weights_only=True,
         verbose=1)
@@ -59,7 +56,7 @@ def train(load_weights=False):
     log_dir = "logs/train/" + datetime.now().strftime("%Y%m%d-%H%M%S")
     tb_callback = tensorflow.keras.callbacks.TensorBoard(log_dir, histogram_freq=1)
 
-    es_callback = tensorflow.keras.callbacks.EarlyStopping(monitor='accuracy', patience=3)
+    es_callback = tensorflow.keras.callbacks.EarlyStopping(monitor='accuracy', patience=5)
 
     model.fit(
         x=dataset_train,
@@ -70,38 +67,5 @@ def train(load_weights=False):
     )
 
 
-def generate_submission():
-    dataset_eval = XRayDatasetGenerator(
-        "dataset_generated/test", "dataset_original/sample_submission.csv", training=False
-    ).get_dataset().batch(BATCH_SIZE)
-
-    model = create_model()
-
-    latest = tensorflow.train.latest_checkpoint("checkpoints/")
-    model.load_weights(latest)
-
-    uids = []
-    targets = []
-
-    for (x, labels) in dataset_eval:
-        preds = model(x)
-        preds = np.rint(preds)
-
-        for label, pred in zip(labels, preds):
-            pred_label = pred.nonzero()[0]
-            pred_label = pred_label + 1  # labels indexing starts from 1
-
-            uids.append(label.numpy().decode())
-            targets.append(" ".join(map(str, pred_label)))
-
-    data = {
-        "SOPInstanceUID": uids,
-        "Target": targets
-    }
-
-    df = pandas.DataFrame(data)
-    df.to_csv("submission.csv", index=False, quoting=csv.QUOTE_NONE)
-
-
 if __name__ == "__main__":
-    generate_submission()
+    train()
